cb74c1e6f3c51c0d281759fad5f29500529f448d
Your Name <your.email@example.com>
2026-03-22 16:56:16 +0700

    complete user crud operation api tested success include pagination

--- src/main/java/com/studentlife/StudentLifeAPIs/Security/SecurityConfig.java
+++ src/main/java/com/studentlife/StudentLifeAPIs/Security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/public?**").permitAll()
+                        .requestMatchers("/public/**").permitAll()
 
                                 .anyRequest().permitAll()
 //                        .anyRequest().authenticated()
