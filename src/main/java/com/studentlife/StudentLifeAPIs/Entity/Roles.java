ed4e364d8f2a019fd3f21807650b699b4bfea1c5
Your Name <your.email@example.com>
2026-03-22 09:16:20 +0700

    complete seed admin and manage role

---
+++ src/main/java/com/studentlife/StudentLifeAPIs/Entity/Roles.java
@@ -0,0 +1,22 @@
+package com.studentlife.StudentLifeAPIs.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Roles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
