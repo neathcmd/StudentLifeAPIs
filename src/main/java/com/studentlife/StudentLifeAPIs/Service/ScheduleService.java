48201bc646b0d43d43ca4e7d43f27245fb9463eb
neathcmd <ksdf71464@gmail.com>
2026-03-25 14:53:36 +0700

    bug

--- src/main/java/com/studentlife/StudentLifeAPIs/Service/ScheduleService.java
+++ src/main/java/com/studentlife/StudentLifeAPIs/Service/ScheduleService.java
@@ -3,11 +3,10 @@ package com.studentlife.StudentLifeAPIs.Service;
 import com.studentlife.StudentLifeAPIs.DTO.Request.ScheduleCreateRequest;
 import com.studentlife.StudentLifeAPIs.DTO.Response.ApiResponse;
 import com.studentlife.StudentLifeAPIs.DTO.Response.PaginatedResponse;
-import com.studentlife.StudentLifeAPIs.DTO.Response.ScheduleResponse;
 
 public interface ScheduleService {
 
-    ApiResponse<PaginatedResponse<ScheduleResponse>> getAllSchedule(int page, int size);
+    PaginatedResponse<ApiResponse<?>> getAllByUser(int page, int size);
 
     ApiResponse<?> createSchedule(ScheduleCreateRequest request);
 }
