a64f602d148fb37028c6bc7f26403548e789abc4
neathcmd <ksdf71464@gmail.com>
2026-03-25 16:14:38 +0700

    complete get all schedule for the admin and still working on the create schedule

--- src/main/java/com/studentlife/StudentLifeAPIs/Service/ScheduleService.java
+++ src/main/java/com/studentlife/StudentLifeAPIs/Service/ScheduleService.java
@@ -1,26 +1,13 @@
 package com.studentlife.StudentLifeAPIs.Service;
 
-import com.studentlife.StudentLifeAPIs.DTO.Request.*;
+import com.studentlife.StudentLifeAPIs.DTO.Request.ScheduleCreateRequest;
 import com.studentlife.StudentLifeAPIs.DTO.Response.ApiResponse;
 import com.studentlife.StudentLifeAPIs.DTO.Response.PaginatedResponse;
 import com.studentlife.StudentLifeAPIs.DTO.Response.ScheduleResponse;
 
-import java.util.List;
-
 public interface ScheduleService {
 
-    ApiResponse<PaginatedResponse<ScheduleResponse>> getByUserId(
-            Long userId,
-            int page,
-            int size,
-            ScheduleFilter filter
-    );
+    ApiResponse<PaginatedResponse<ScheduleResponse>> getAllSchedule(int page, int size);
 
-    ApiResponse<ScheduleResponse> getById(Long scheduleId);
-
-    ApiResponse<ScheduleResponse> createOneTime(OneTimeScheduleRequest request);
-
-    ApiResponse<ScheduleResponse> createRecurring(RecurringScheduleRequest request);
-
-    ApiResponse<ScheduleResponse> updateSchedule(Long scheduleId, ScheduleUpdateRequest request);
+    ApiResponse<?> createSchedule(ScheduleCreateRequest request);
 }
