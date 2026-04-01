51e15399dfb518717dbc12be179d8a27fab75392
neathcmd <ksdf71464@gmail.com>
2026-04-01 11:12:21 +0700

    fix email strict issue

--- src/main/java/com/studentlife/StudentLifeAPIs/DTO/Request/RegisterRequest.java
+++ src/main/java/com/studentlife/StudentLifeAPIs/DTO/Request/RegisterRequest.java
@@ -1,14 +1,17 @@
 package com.studentlife.StudentLifeAPIs.DTO.Request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-
-import java.util.List;
+import jakarta.validation.constraints.Email;
 
 @Data
 public class RegisterRequest {
 
     private String fullname;
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
     private String password;
     private String phone;
