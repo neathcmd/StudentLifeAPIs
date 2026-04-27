733247c080e1b7f21fa77d0f42eff1205a3e31af
neathcmd <ksdf71464@gmail.com>
2026-04-27 09:10:39 +0700

    include a full path

--- src/main/java/com/studentlife/StudentLifeAPIs/Config/WebSocketConfig.java
+++ src/main/java/com/studentlife/StudentLifeAPIs/Config/WebSocketConfig.java
@@ -22,7 +22,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/api/v1/ws")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
